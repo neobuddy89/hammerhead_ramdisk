127ef498eb121f7c2a8052a6ebe15fd55628f922
Pranav Vashi <neobuddy89@gmail.com>
2014-09-17 20:11:04 +0530

    ramdisk: Update
    * Remove NXTweaks support.
    * Add Synapse backend.
    * Complete new ramdisk.
    * Special thanks to apbaxel for UKM which is bible
      of Synapse implementation techniques. Helped a lot
      to understand how to make it work perfectly.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ sbin/post-init.sh
@@ -0,0 +1,23 @@
+#!/sbin/busybox sh
+
+BB=/sbin/busybox
+
+# Mount root as RW to apply tweaks and settings
+$BB mount -t rootfs -o remount,rw rootfs;
+$BB mount -o remount,rw /;
+
+# Make tmp folder
+$BB mkdir /tmp;
+
+# Give permissions to execute
+$BB chown -R root:system /tmp/;
+$BB chmod -R 777 /tmp/;
+$BB chmod -R 777 /res/;
+$BB chmod 6755 /sbin/*;
+$BB echo "Boot initiated on $(date)" > /tmp/bootcheck;
+
+$BB echo "1536,2048,4096,16384,28672,32768" > /sys/module/lowmemorykiller/parameters/minfree
+$BB echo 32 > /sys/module/lowmemorykiller/parameters/cost
+
+ln -s /res/synapse/uci /sbin/uci
+/sbin/uci
