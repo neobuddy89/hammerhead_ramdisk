924369a093d79262307552bef9be582cc2b9866f
Pranav Vashi <neobuddy89@gmail.com>
2015-01-26 12:12:04 +0530

    init: SELinux tweaks
    * Experimental.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -15,6 +15,20 @@ if [ -e /system/bin/thermal-engine-hh ]; then
 mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh-bak;
 fi;
 
+# allow untrusted apps to read from debugfs
+/system/xbin/supolicy --live \
+	"allow untrusted_app debugfs file { open read getattr }" \
+	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
+	"allow untrusted_app persist_file dir { open read getattr }" \
+	"allow debuggerd gpu_device chr_file { open read getattr }" \
+	"allow netd netd capability fsetid" \
+	"allow netd { hostapd dnsmasq } process fork" \
+	"allow { system_app shell } dalvikcache_data_file file write" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
+	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }"\
+	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }"
+
 # Make tmp folder
 $BB mkdir /tmp;
 
