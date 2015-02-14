e0d4323f7d05a1ac86e20e039fba60ed285c82a4
Pranav Vashi <neobuddy89@gmail.com>
2015-02-14 13:46:07 +0530

    init: Update
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -25,8 +25,11 @@ rm -rf /data/UKM;
 	"allow { system_app shell } dalvikcache_data_file file write" \
 	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
 	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
-	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }"\
-	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }"
+	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
+	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
+	"allow system_server dex2oat_exec file rx_file_perms" \
+	"allow mediaserver mediaserver_tmpfs file execute" \
+	"allow drmserver theme_data_file file r_file_perms"
 
 # Make tmp folder
 mkdir /tmp;
