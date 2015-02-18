e73518c94cc14b89ca7e7e4b218142057ae99d54
Pranav Vashi <neobuddy89@gmail.com>
2015-02-18 22:31:20 +0530

    ramdisk: Use Impulse as default governor
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -20,6 +20,7 @@ rm -rf /data/UKM;
 # allow untrusted apps to read from debugfs
 /system/xbin/supolicy --live \
 	"allow untrusted_app debugfs file { open read getattr }" \
+	"allow untrusted_app sysfs_hardware file { open read getattr }" \
 	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
 	"allow untrusted_app persist_file dir { open read getattr }" \
 	"allow debuggerd gpu_device chr_file { open read getattr }" \
